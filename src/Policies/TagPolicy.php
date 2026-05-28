270baa6443984e01d11a86e2e552b37ad5e35ccd
Ozan Kurt <me@ozankurt.com>
2026-05-28 17:54:21 +0300

    feat(blog): add Post, Comment, Category, Tag policies

---
+++ src/Policies/TagPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kurt\Modules\Blog\Policies;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Kurt\Modules\Blog\Models\Tag;
+
+final class TagPolicy
+{
+    public function view(?Authenticatable $user, Tag $tag): bool
+    {
+        return true;
+    }
+
+    public function create(Authenticatable $user): bool
+    {
+        return $this->isStaff($user);
+    }
+
+    public function update(Authenticatable $user, Tag $tag): bool
+    {
+        return $this->isStaff($user);
+    }
+
+    public function delete(Authenticatable $user, Tag $tag): bool
+    {
+        return $this->isStaff($user);
+    }
+
+    private function isStaff(Authenticatable $user): bool
+    {
+        return app('gate')->allows('canManageBlog', $user);
+    }
+}
